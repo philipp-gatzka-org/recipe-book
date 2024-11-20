9b5ed28b1fa3203b2230098215c6f8b1b7d2c21d
philipp-gatzka <phil.gatzka@gmail.com>
2024-11-20 15:16:29 +0100

    gh-5 setup jooq codegeneration

--- metamodel/build.gradle.kts
+++ metamodel/build.gradle.kts
@@ -1,13 +1,24 @@
 import org.flywaydb.gradle.task.FlywayMigrateTask
+import org.jooq.meta.jaxb.MatcherTransformType
 
 plugins {
+    id("java-library")
     alias(libs.plugins.flyway)
+    alias(libs.plugins.jooq)
 }
 
-val migrationUrl: String by project
-val migrationUser: String by project
-val migrationPassword: String by project
-val migrationSchema: String by project
+dependencies {
+    implementation(platform(libs.spring.boot.dependencies))
+    jooqCodegen(platform(libs.spring.boot.dependencies))
+    implementation(libs.jooq)
+    jooqCodegen(libs.database.h2)
+}
+
+val h2Connection = "jdbc:h2:file:~/recipe-book;MODE=PostgreSQL"
+val migrationUrl = findProperty("migrationUrl") as? String ?: h2Connection
+val migrationUser = findProperty("migrationUser") as? String ?: "sa"
+val migrationPassword = findProperty("migrationPassword") as? String ?: ""
+val migrationSchema = findProperty("migrationSchema") as? String ?: "recipe-book"
 
 flyway {
     url = migrationUrl
@@ -17,13 +28,70 @@ flyway {
     locations = arrayOf("filesystem:src/main/resources/db/postgres")
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+    sourceSets["main"].java {
+        srcDir("build/generated-sources/jooq")
+    }
+}
+
+jooq {
+    configuration {
+        jdbc {
+            driver = "org.h2.Driver"
+            url = h2Connection
+            user = migrationUser
+            password = migrationPassword
+        }
+        generator {
+            database {
+                name = "org.jooq.meta.h2.H2Database"
+                inputSchema = migrationSchema
+                isIncludeSequences = true
+                isIncludeSystemSequences = true
+                excludes = """
+                    FLYWAY_SCHEMA_HISTORY
+                """.trimIndent()
+            }
+            generate {
+                isFluentSetters = true
+                isSequences = true
+                strategy {
+                    matchers {
+                        tables {
+                            table {
+                                tableClass {
+                                    transform = MatcherTransformType.PASCAL
+                                    expression = "$0_Table"
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            target {
+                packageName = project.group.toString()
+            }
+        }
+    }
+}
+
 tasks {
     register<FlywayMigrateTask>("flywayMigrateH2") {
-        url = "jdbc:h2:mem:recipe-book;MODE=PostgreSQL;DB_CLOSE_ON_EXIT=FALSE"
+        doFirst {
+            val userHome = System.getProperty("user.home")
+            File(userHome, "recipe-book.mv.db").delete()
+        }
+
+        url = migrationUrl
         user = migrationUser
         password = migrationPassword
         schemas = arrayOf(migrationSchema)
         locations = arrayOf("filesystem:src/main/resources/db/h2")
+    }
+    jooqCodegen {
+        dependsOn(named("flywayMigrateH2"))
     }
 }
 
--- metamodel/src/main/resources/db/h2/V1_0_0__MIGRATION_001.sql
+++ metamodel/src/main/resources/db/h2/V1_0_0__MIGRATION_001.sql
@@ -1,13 +1,15 @@
 -- Create tables
+create sequence account_id_seq;
+
 create table account
 (
-    id                      serial                              not null,
-    creation_timestamp      timestamp default CURRENT_TIMESTAMP not null,
-    modification_timestamp  timestamp default CURRENT_TIMESTAMP not null,
-    firstname               varchar(50)                         not null,
-    lastname                varchar(50)                         not null,
-    email                   varchar(255)                        not null,
-    password                varchar(255)                        not null,
+    id                      int       default next value for account_id_seq not null,
+    creation_timestamp      timestamp default CURRENT_TIMESTAMP             not null,
+    modification_timestamp  timestamp default CURRENT_TIMESTAMP             not null,
+    firstname               varchar(50)                                     not null,
+    lastname                varchar(50)                                     not null,
+    email                   varchar(255)                                    not null,
+    password                varchar(255)                                    not null,
     email_verification_code varchar(50),
     password_reset_code     varchar(50),
     constraint pk_account__id
